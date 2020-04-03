af7c0eae7859332c368cf0ccdf0e71164be871b7
s18289 <s18289@pjwstk.edu.pl>
2020-04-03 13:28:51 +0200

    commit

---
+++ WebApplication1/Controllers/EnrollmentsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DAL;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/enrollment")]
+    public class EnrollmentsController : Controller
+    {
+        private IDbService _dbService;
+
+        public EnrollmentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        
+    }
+}
